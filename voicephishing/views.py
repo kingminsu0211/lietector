57be15279782b01fb1369c0f2de93e5c208afd54
kingminsu0211 <sleah729@hanyang.ac.kr>
2024-02-08 00:50:31 +0900

    'voice_swagger'

--- voicephishing/views.py
+++ voicephishing/views.py
@@ -3,12 +3,30 @@ from .serializers import *
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 
 class DiagnosisListView(generics.ListCreateAPIView):
     queryset = Diagnosis.objects.all()
     serializer_class = DiagnosisSerializer
 
+# 통화 진단하기 API에 대한 Swagger 문서
+@swagger_auto_schema(
+    method='post',
+    operation_description="통화 진단하기 API",
+    request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        required=['diagnosis_type'],
+        properties={
+            'diagnosis_type': openapi.Schema(type=openapi.TYPE_STRING, description='진단 타입'),
+            'call_details': openapi.Schema(type=openapi.TYPE_STRING, description='통화 내용'),
+            'audio_file': openapi.Schema(type=openapi.TYPE_FILE, description='통화 녹음 파일'),
+            'suspicion_percentage': openapi.Schema(type=openapi.TYPE_NUMBER, description='의심도(퍼센트)'),
+        },
+    ),
+    responses={200: '성공적으로 진단됨', 400: '잘못된 요청'},
+)
 # 통화 진단하기
 @api_view(['POST'])
 def diagnose_voice(request):
