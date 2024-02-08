0f3e7ef3f35d89a1a0225f6fcc9eab467ea8408c
kingminsu0211 <sleah729@hanyang.ac.kr>
2024-02-08 15:28:39 +0900

    'statics'

--- lietector1/urls.py
+++ lietector1/urls.py
@@ -16,14 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path, re_path
-from django.conf import settings
 
 from rest_framework import routers
 from rest_framework_swagger.views import get_swagger_view
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+from django.conf.urls.static import static
+from django.conf import settings
 schema_view = get_schema_view(
     openapi.Info(
         title="Your API Title",
@@ -45,7 +45,7 @@ urlpatterns = [
     path('', include('community.urls')),
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 if settings.DEBUG:
